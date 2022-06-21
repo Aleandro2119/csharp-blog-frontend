89e6eec9f28ab41454ef5d8baf4042c286aa1dba
Aleandro2119 <aleandro.basilico@gmail.com>
2022-06-21 14:48:55 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using csharp_blog_frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace csharp_blog_frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(id);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult CreateBlob(int id)
+        {
+            return View(id);
+        }
+        //public IActionResult CreateFile()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+    }
+}
